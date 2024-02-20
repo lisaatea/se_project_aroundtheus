a39377722377db1a9dea59d0eaa84fb47f10186c
lisaatea <lisaatea@gmail.com>
2024-02-20 05:40:07 -0800

    project 5 update 1

--- scripts/index.js
+++ scripts/index.js
@@ -75,6 +75,7 @@ function getCardElement(cardData) {
 
   cardImageElement.addEventListener("click", () => {
     modalImageElement.src = cardData.link;
+    modalImageElement.alt = "image of ${cardData.name}";
     modalCaption.textContent = cardData.name;
     openModal(imageModal);
   });
